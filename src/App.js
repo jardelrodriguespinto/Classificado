dbc10fd14d5cce645c84b00d524e596b27538773
jardelrodriguespinto <ghmagno910@gmail.com>
2023-11-30 21:23:19 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import  NavbarPersonalizada from "./components/NavbarPersonalizada";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const App = () => {
+  return (
+  <>
+  <NavbarPersonalizada/>
+  </>);
+};
+
+export default App;
