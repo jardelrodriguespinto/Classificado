b8e49465128bfdb840aa4c0fc0c5bd16b41b76f9
jardel.rodrigues <jardel.rodrigues@neomind.com>
2023-12-02 23:22:19 -0300

    adicionando o footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import  MainHome from "./MainHome";
 import  Header from "./Header";
+import  Footer from "./Footer";
 
 function Home(){
   return (
   <>
     <Header/>
     <MainHome/>
+    <Footer/>
   </>);
 };
 
